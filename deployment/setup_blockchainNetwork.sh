a0d9bb40516330d67a3fc70ec6b330682aa15d90
Harsha Vamsi <harshavamsi096@gmail.com>
2022-04-02 15:09:32 -0700

    fix versions

--- deployment/setup_blockchainNetwork.sh
+++ deployment/setup_blockchainNetwork.sh
@@ -195,21 +195,21 @@ echo "Chaincode Install Completed Successfully"
 
 
 # Instantiate chaincode on channel
-echo -e "\nCreating chaincodeinstantiate job"
-echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml"
-kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml
+# echo -e "\nCreating chaincodeinstantiate job"
+# echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml"
+# kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml
 
-JOBSTATUS=$(kubectl get jobs |grep chaincodeinstantiate |awk '{print $2}')
-while [ "${JOBSTATUS}" != "1/1" ]; do
-    echo "Waiting for chaincodeinstantiate job to be completed"
-    sleep 1;
-    if [ "$(kubectl get pods | grep chaincodeinstantiate | awk '{print $3}')" == "Error" ]; then
-        echo "Chaincode Instantiation Failed"
-        exit 1
-    fi
-    JOBSTATUS=$(kubectl get jobs |grep chaincodeinstantiate |awk '{print $2}')
-done
-echo "Chaincode Instantiation Completed Successfully"
+# JOBSTATUS=$(kubectl get jobs |grep chaincodeinstantiate |awk '{print $2}')
+# while [ "${JOBSTATUS}" != "1/1" ]; do
+#     echo "Waiting for chaincodeinstantiate job to be completed"
+#     sleep 1;
+#     if [ "$(kubectl get pods | grep chaincodeinstantiate | awk '{print $3}')" == "Error" ]; then
+#         echo "Chaincode Instantiation Failed"
+#         exit 1
+#     fi
+#     JOBSTATUS=$(kubectl get jobs |grep chaincodeinstantiate |awk '{print $2}')
+# done
+# echo "Chaincode Instantiation Completed Successfully"
 
 sleep 15
 echo -e "\nNetwork Setup Completed !!"
